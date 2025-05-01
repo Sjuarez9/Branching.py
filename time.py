387e5fd794062274ba490aafc45716248c27d7be
Sjuarez9 <salvadorjuarez525@gmail.com>
2025-05-01 14:50:04 -0700

    Update time.py

--- time.py
+++ time.py
@@ -1,8 +1,8 @@
 currentTimeStr = input("What is the current time (in hours 0-23)?")
 waitTimeStr = input("How many hours do you want to wait"
 
-currentTimeInt = int(current_time_str)
-waitTimeInt = int(wait_time_str)
+currentTimeInt = int(currentTimeStr)
+waitTimeInt = int(waitTimeItr)
 
 finalTimeInt = currentTimeInt + waitTimeInt
-print(finalTime_Int)
+print(finalTimeInt)
